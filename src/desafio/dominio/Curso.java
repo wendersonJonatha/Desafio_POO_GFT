c1a0052dc02932e3676e2cd66cc0f16feeabf2ee
wenderson jonatha <wendersonkw1@gmail.com>
2025-07-28 18:55:21 -0300

    criação da classe Curso.java

---
+++ src/desafio/dominio/Curso.java
@@ -0,0 +1,33 @@
+package desafio.dominio;
+
+
+
+public class Curso  extends Conteudo {
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
